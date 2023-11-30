453d73d1e8c68d8b277ff0012eb99b74973295f6
Sreerag9121 <151636002+Sreerag9121@users.noreply.github.com>
2023-11-30 15:52:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+function changeimg(x,image)
+{
+    if(x==1){
+        image.src='./images/imd1-1-1.webp'
+    }
+    if(x==2){
+        image.src='./images/img1-1.webp' 
+    }
+
+}
+function changeimg2(x,image)
+{
+    if(x==1){
+        image.src='./images/img1-2-2.webp'
+    }
+    if(x==2){
+        image.src='./images/img1-2.webp' 
+    }
+
+}
+function changeimg3(x,image)
+{
+    if(x==1){
+        image.src='./images/img1-3-3.webp'
+    }
+    if(x==2){
+        image.src='./images/img1-3.webp' 
+    }
+
+}
+function changeimg4(x,image)
+{
+    if(x==1){
+        image.src='./images/img2-1-1.webp'
+    }
+    if(x==2){
+        image.src='./images/img2-1.webp' 
+    }
+
+}
+function changeimg5(x,image)
+{
+    if(x==1){
+        image.src='./images/img2-2-2.webp'
+    }
+    if(x==2){
+        image.src='./images/img2-2.webp' 
+    }
+
+}
+function changeimg6(x,image)
+{
+    if(x==1){
+        image.src='./images/img2-3-3.webp'
+    }
+    if(x==2){
+        image.src='./images/img2-3.webp' 
+    }
+
+}
+function changeimg7(x,image)
+{
+    if(x==1){
+        image.src='./images/img3-1-1.webp'
+    }
+    if(x==2){
+        image.src='./images/img3-1.webp' 
+    }
+
+}
+function changeimg8(x,image)
+{
+    if(x==1){
+        image.src='./images/img3-2-2.webp'
+    }
+    if(x==2){
+        image.src='./images/img3-2.webp' 
+    }
+
+}
+function changeimg9(x,image)
+{
+    if(x==1){
+        image.src='./images/img3-3-3.webp'
+    }
+    if(x==2){
+        image.src='./images/img3-3.webp' 
+    }
+
+}
